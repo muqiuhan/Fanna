338b8f565049706d4bfa3e0d99629136a5d88f5a
Muqiu Han <muqiu-han@outlook.com>
2023-05-08 11:33:34 +0800

    Refactor

--- bin/main.ml
+++
@@ -1,9 +0,0 @@
-open Core
-
-let _ =
-  try
-    In_channel.read_all (Sys.get_argv ()).(1)
-    |> Fanna.Lexer.to_token_list
-    |> List.map ~f:Fanna.Lexer.Token.show
-    |> List.iter ~f:print_endline
-  with Failure e -> print_endline e
